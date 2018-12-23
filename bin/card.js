cad1139917fd6ee448d749bbbf6a6c3a562b0f80
Yaser Adel Mehraban <adel.mehrban@gmail.com>
2018-12-23 12:24:54 +1100

    Changed styling

--- bin/card.js
+++ bin/card.js
@@ -20,41 +20,38 @@ const options = {
 
 // Text + chalk definitions
 const data = {
-	name: chalk.white( "Yaser Adel Mehraban" ),
-	handle: chalk.cyan( "yashints" ),
-	work: chalk.white( "Lead Consultant at Readify" ),
-	workUrl: chalk.cyan( "https://readify.net" ),
-	twitter: chalk.cyan( "https://twitter.com/yashints" ),
-	github: chalk.cyan( "https://github.com/yashints" ),
-	linkedin: chalk.cyan( "https://linkedin.com/in/yashints" ),
-	web: chalk.cyan( "https://mehraban.com.au.com" ),
-	npx: chalk.white( "npx yashints" ),
+	name: chalk.magenta.bold( "Yaser Adel Mehraban" ),
+	handle: chalk.magenta.bold( "yashints" ),
+	work: chalk.green( "Lead Consultant at Readify" ),
+	twitter: chalk.cyan( "https://twitter.com/" ),
+	github: chalk.cyan( "https://github.com/" ),
+	linkedin: chalk.cyan( "https://linkedin.com/in/" ),
+	web: chalk.yellow( "https://mehraban.com.au.com" ),
+	npx: chalk.cyan( "npx yashints" ),
 	labelWork: chalk.white.bold( "      Work:" ),
-	labelWorkUrl: chalk.white.bold( "           " ),
 	labelTwitter: chalk.white.bold( "   Twitter:" ),
 	labelGitHub: chalk.white.bold( "    GitHub:" ),
 	labelLinkedIn: chalk.white.bold( "  LinkedIn:" ),
 	labelWeb: chalk.white.bold( "       Web:" ),
 	labelCard: chalk.white.bold( "      Card:" ),
 	bio: chalk.white( `This man whose name only looks hard to say is a 
-	creative web dev, speaker, blogger, hiker and a Die Hard fan, 
-	but don’t hold this against him.` )
+creative web dev, speaker, blogger, hiker and a Die Hard fan, 
+but don’t hold this against him.` )
 };
 
 // Actual strings we're going to output
 const heading = `             ${ data.name }`;
 const working = `\n${ data.labelWork }  ${ data.work }`;
-const workingUrl = `${ data.labelWorkUrl }  ${ data.workUrl }`;
-const twittering = `${ data.labelTwitter }  ${ data.twitter }`;
-const githubing = `${ data.labelGitHub }  ${ data.github }`;
-const linkedining = `${ data.labelLinkedIn }  ${ data.linkedin }`;
+const twittering = `${ data.labelTwitter }  ${ data.twitter }${data.handle}`;
+const githubing = `${ data.labelGitHub }  ${ data.github }${data.handle}`;
+const linkedining = `${ data.labelLinkedIn }  ${ data.linkedin }${data.handle}`;
 const webing = `${ data.labelWeb }  ${ data.web }`;
 const carding = `\n${ data.labelCard }  ${ data.npx }`;
 const bio = `\n${ data.bio }`;
 
 const card = [
-	avatar, banner,
-	heading, working, workingUrl,
+	banner,
+	heading, working, 
 	twittering, githubing, linkedining,
 	webing, carding, bio
 ];
